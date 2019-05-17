3bf329532b97e542f722acb9da5f013c604243a7
mgodu01 <44791606+mgodu01@users.noreply.github.com>
2019-05-16 21:46:17 -0700

    first commit

---
+++ server.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const fs = require('fs');
+const app = express();
+const ejs = require('ejs');
+const props=require('./properties.json');
+
+app.use('/dist',express.static('dist'));
+app.use('/header', express.static('header'));
+
+app.set('view engine', 'html');
+app.engine('html', ejs.renderFile);
+app.set('views', __dirname + '/dist');
+
+app.get('/**', (req,res) => {
+    console.log('info', 'Hostname is: ' + req.hostname);
+    console.log('info', 'Req URL: ' +  req.url)
+    console.log('info', 'Headers: ' + JSON.stringify(req.headers));
+    // banner
+    let banner = 'safeway';
+    
+
+    console.log('info','Banner is: ' + banner);
+    res.render('index', {
+        banner: banner,
+        footerFragment: props.header
+    });
+});
+
+app.listen(process.env.PORT || 3000, () => {
+    console.log('info', 'Server started on port: ' + (process.env.PORT || 3000));
+    });
